b2b301ab2231ddc153eedd7648ce3bc55f833bd9
czyangyang <842988376@qq.com>
2015-07-22 21:59:29 +0800

    1.0
    创建

---
+++ YYFrame/YYFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYFrame
+//
+//  Created by 杨阳 on 15/7/22.
+//  Copyright (c) 2015年 杨阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYFrame/YYFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYFrame
+//
+//  Created by 杨阳 on 15/7/22.
+//  Copyright (c) 2015年 杨阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
